6c88c8434208d941e75ee7e2e80925c0df3b2ef8
Guru <sjb.bhatt@gmail.com>
2019-04-28 23:04:51 -0700

    Updated the script to display all data

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,4 +1,15 @@
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
 // from data.js
 var tableData = data;
 
 // YOUR CODE HERE!
+tableData.forEach((AllUFOData) => {
+    var row = tbody.append("tr");
+    Object.entries(AllUFOData).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+  
