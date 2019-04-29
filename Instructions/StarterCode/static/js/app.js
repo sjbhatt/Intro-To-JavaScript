8f18e3b1f26e202ec4dadec44b59cdb8d4071fed
Guru <sjb.bhatt@gmail.com>
2019-04-28 23:44:15 -0700

    Updated the script to add filter data

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -13,3 +13,24 @@ tableData.forEach((AllUFOData) => {
     });
   });
   
+// Select the submit button
+var submit = d3.select("#filter-btn");
+
+submit.on("click", function() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+//   console.log(inputValue);
+//   console.log(people);
+
+//   var filteredData = people.filter(person => person.bloodType === inputValue);
+
+//   console.log(filteredData);
+});
