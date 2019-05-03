70b50070889c8039ef23662360dde18acb4f83f9
Guru <sjb.bhatt@gmail.com>
2019-05-02 23:30:36 -0700

    Final Changes

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -3,34 +3,43 @@ var tbody = d3.select("tbody");
 
 // from data.js
 var tableData = data;
+console.log(tableData);
 
-// YOUR CODE HERE!
-tableData.forEach((AllUFOData) => {
+// Level One Part 2
+function table_obj_html_fmt(dataset){
+  tbody.text("");
+  dataset.forEach((AllUFOData) => {
     var row = tbody.append("tr");
     Object.entries(AllUFOData).forEach(([key, value]) => {
       var cell = row.append("td");
       cell.text(value);
     });
   });
-  
+}
+
+table_obj_html_fmt(tableData);
+
+// Level One Part 3
 // Select the submit button
 var submit = d3.select("#filter-btn");
 
-submit.on("click", function() {
 
+// Submit Button handler
+function handleSubmit(){
   // Prevent the page from refreshing
   d3.event.preventDefault();
-
   // Select the input element and get the raw HTML node
-  var inputElement = d3.select("#datetime");
-
+  var filter_Element = d3.select("#datetime")
   // Get the value property of the input element
-  var inputValue = inputElement.property("value");
+  var filter_Value =  filter_Element.property("value");
+  console.log(filter_Value);
 
-//   console.log(inputValue);
-//   console.log(people);
+  //Create a new table showing only the filterd data
+  var filter_by_datetime = tableData.filter(FilteredUFOData => FilteredUFOData.datetime===filter_Value);
+  console.log(filter_by_datetime);
+  //Display the new table
+  table_obj_html_fmt(filter_by_datetime);
+}
 
-//   var filteredData = people.filter(person => person.bloodType === inputValue);
-
-//   console.log(filteredData);
-});
+// Add event listener for submit button
+submit.on("click", handleSubmit);
